b9dabbc791dc3fb601c14b6a0d3294b5d8af82d8
Federated Secure Computing <federatedsecurecomputing@lrz.uni-muenchen.de>
2023-01-08 17:16:35 +0100

    import precursor codebase (renamed and sanitized)

---
+++ src/federatedsecure/webserver/connexion/__main__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+the main module
+"""
+
+import sys
+import connexion
+import waitress
+
+from flask import current_app
+
+from federatedsecure.server.server.bus import Bus
+from federatedsecure.webserver.connexion.encoder import JSONEncoder
+
+
+def main():
+    """runs the webserver"""
+
+    # enable the following line to log endpoint traffic
+    # logging.basicConfig(level=logging.INFO)
+
+    port = 8080
+    for arg in sys.argv:
+        if arg[:7] == "--port=":
+            port = int(arg[7:])
+
+    bus = Bus()
+
+    app = connexion.App(__name__, specification_dir='openapi/')
+    app.app.json_encoder = JSONEncoder
+    app.add_api('openapi.yaml',
+                arguments={'title': 'Federated Secure Computing'},
+                pythonic_params=True)
+
+    with app.app.app_context():
+        current_app.bus = bus
+
+    @app.app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
+        return response
+
+    waitress.serve(app, host="0.0.0.0", port=port)
+
+
+if __name__ == '__main__':
+    main()
